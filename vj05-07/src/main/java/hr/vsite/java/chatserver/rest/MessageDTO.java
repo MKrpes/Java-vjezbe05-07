95a66ad51bfb57412c8f02a28dc17e734347caea
Mihael Krpes <147085973+MKrpes@users.noreply.github.com>
2024-02-17 23:47:18 +0100

    Add files via upload

---
+++ vj05-07/src/main/java/hr/vsite/java/chatserver/rest/MessageDTO.java
@@ -0,0 +1,38 @@
+package hr.vsite.java.chatserver.rest;
+
+import hr.vsite.java.chatserver.db.Group;
+import hr.vsite.java.chatserver.db.User;
+
+public class MessageDTO {
+    private String message;
+    private User user;
+    private Group group;
+
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    public Group getGroup() {
+        return group;
+    }
+    public void setGroup(Group group) {
+        this.group = group;
+    }
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "message='" + message + '\'' +
+                ", user=" + user +
+                ", group=" + group +
+                '}';
+    }
+}
